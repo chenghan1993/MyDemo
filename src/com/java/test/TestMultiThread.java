78735d804fe8418c3a9fce5cd1ee6d54ee2b0922
chenghan1993 <chenghan1993@163.com>
2018-04-20 18:27:28 +0800

    使用线程池，周期或定时执行任务

--- src/com/java/test/TestMultiThread.java
+++ src/com/java/test/TestMultiThread.java
@@ -1,38 +1,7 @@
 package com.java.test;
 
-class Thread4 implements Runnable {
-
-	private String name;
-	private Object self;
-	private Object next;
-
-	public Thread4(String name, Object self, Object next) {
-		this.name = name;
-		this.self = self;
-		this.next = next;
-	}
-
-	@Override
-	public void run() {
-		int count = 10;
-		while (count > 0) {
-			synchronized (self) {
-				synchronized (next) {
-					System.out.println(name);
-					count--;
-					self.notify();
-
-				}
-				try {
-					next.wait();
-				} catch (InterruptedException e) {
-					e.printStackTrace();
-				}
-			}
-		}
-	}
-
-}
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 
 class Thread3 implements Runnable {
 
@@ -121,11 +90,19 @@ public class TestMultiThread {
 		Object c = new Object();
 		Object d = new Object();
 		Object e = new Object();
-		Thread4 ta = new Thread4("A", a, b);
-		Thread4 tb = new Thread4("B", b, c);
-		Thread4 tc = new Thread4("C", c, d);
-		Thread4 td = new Thread4("D", d, e);
-		Thread4 te = new Thread4("E", e, a);
+		Thread3 ta = new Thread3("A", e, a);
+		Thread3 tb = new Thread3("B", a, b);
+		Thread3 tc = new Thread3("C", b, c);
+		Thread3 td = new Thread3("D", c, d);
+		Thread3 te = new Thread3("E", d, e);
+		// 行不通
+		// ExecutorService exec = Executors.newSingleThreadExecutor();
+		// Thread3[] objArr = new Thread3[] { ta, tb, tc, td, te };
+		// for (int i = 0; i < objArr.length; i++) {
+		// exec.execute(objArr[i]);
+		// }
+		// exec.shutdown();
+
 		new Thread(ta).start();
 		Thread.sleep(100);
 		new Thread(tb).start();
@@ -136,28 +113,6 @@ public class TestMultiThread {
 		Thread.sleep(100);
 		new Thread(te).start();
 		Thread.sleep(100);
-
-		// Object a = new Object();
-		// Object b = new Object();
-		// Object c = new Object();
-		// Object d = new Object();
-		// Object e = new Object();
-		// Thread3 ta = new Thread3("A", e, a);
-		// Thread3 tb = new Thread3("B", a, b);
-		// Thread3 tc = new Thread3("C", b, c);
-		// Thread3 td = new Thread3("D", c, d);
-		// Thread3 te = new Thread3("E", d, e);
-
-		// new Thread(ta).start();
-		// Thread.sleep(100);
-		// new Thread(tb).start();
-		// Thread.sleep(100);
-		// new Thread(tc).start();
-		// Thread.sleep(100);
-		// new Thread(td).start();
-		// Thread.sleep(100);
-		// new Thread(te).start();
-		// Thread.sleep(100);
 
 		// Thread1 th1 = new Thread1("A");
 		// Thread1 th2 = new Thread1("B");
--- src/com/java/test/TestThreadPool.java
+++ src/com/java/test/TestThreadPool.java
@@ -1,20 +1,150 @@
 package com.java.test;
 
+import java.text.DateFormat;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.ScheduledThreadPoolExecutor;
+import java.util.concurrent.TimeUnit;
 
-public class TestThreadPool implements Runnable {
-
-	private static final int POOL_SIZE = 10;
-
-	ExecutorService executorService1 = Executors.newCachedThreadPool();
-
-	int cpuNums = Runtime.getRuntime().availableProcessors();
-	ExecutorService executorService2 = Executors.newFixedThreadPool(cpuNums * POOL_SIZE);
+class Handle2 implements Runnable {
 
 	@Override
 	public void run() {
-
+		System.out.println(System.currentTimeMillis());
+		try {
+			Thread.sleep(1 * 1000);
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		}
 	}
 
+}
+
+/**
+ * newFixedThreadPool 创建一个固定长度的线程池，当到达线程最大数量时，线程池的规模将不再变化
+ * <p>
+ * newCachedThreadPool 创建一个可缓存的线程池，如果当前线程池的规模超出了处理需求，将回收空的线程；
+ * 当需求增加时，会增加线程数量；线程池规模无限制
+ * </p>
+ * newSingleThreadPoolExecutor 创建一个单线程的Executor，确保任务对了，串行执行
+ * newScheduledThreadPool 创建一个固定长度的线程池，而且以延迟或者定时的方式来执行，类似Timer
+ * 
+ * @author ChengHan
+ * @date 2018年4月20日 下午4:26:22
+ */
+class Handle implements Runnable {
+
+	private String name;
+
+	public Handle(String name) {
+		this.name = "Thread" + name;
+	}
+
+	@Override
+	public void run() {
+		System.out.println(name + " Start. Time = " + new Date());
+		processCommand();
+		System.out.println(name + " End. Time = " + new Date());
+	}
+
+	private void processCommand() {
+		try {
+			Thread.sleep(1000);
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		}
+	}
+
+	public String toString() {
+		return this.name;
+	}
+
+}
+
+public class TestThreadPool {
+	public static void main(String[] args) {
+		// executeFixedRate();
+		// executorFixedDelay();
+		executeEightAtNightPerDay();
+	}
+
+	/**
+	 * 按指定频率周期执行任务，间隔指的是两次任务开始执行时间的间隔
+	 * 当执行任务所需时间>间隔时间，并不会开辟新线程并发的执行任务，而是等待该线程执行完毕后，立即执行
+	 * 
+	 * @author ChengHan
+	 * @date 2018年4月20日 下午5:47:48
+	 */
+	private static void executeFixedRate() {
+		ScheduledExecutorService exec = Executors.newScheduledThreadPool(10);
+		exec.scheduleAtFixedRate(new Handle2(), 0, 2000, TimeUnit.MILLISECONDS);
+	}
+
+	/**
+	 * 按指定频率间隔执行某个任务，间隔值得是上次执行完成和下次执行开始时间的间隔
+	 * 
+	 * @author ChengHan
+	 * @date 2018年4月20日 下午6:03:42
+	 */
+	private static void executorFixedDelay() {
+		ScheduledExecutorService exec = Executors.newScheduledThreadPool(10);
+		exec.scheduleWithFixedDelay(new Handle2(), 0, 2000, TimeUnit.MILLISECONDS);
+	}
+
+	/**
+	 * 周期性的执行任务，下面方法设定每天固定9点执行一次任务
+	 * 
+	 * @author ChengHan
+	 * @date 2018年4月20日 下午6:23:43
+	 */
+	private static void executeEightAtNightPerDay() {
+		ScheduledExecutorService exec = Executors.newScheduledThreadPool(1);
+		long oneDay = 24 * 60 * 60 * 1000;
+		long initDelay = getTimeMillis("18:00:00") - System.currentTimeMillis();
+		initDelay = initDelay > 0 ? initDelay : oneDay + initDelay;
+		exec.scheduleAtFixedRate(new Handle2(), initDelay, oneDay, TimeUnit.MILLISECONDS);
+	}
+
+	/**
+	 * 获取指定时间对应的毫秒数
+	 * 
+	 * @author ChengHan
+	 * @date 2018年4月20日 下午6:08:41
+	 * @param time
+	 * @return
+	 */
+	private static long getTimeMillis(String time) {
+		try {
+			DateFormat dateFormat = new SimpleDateFormat("yy-MM-dd HH:mm:ss");
+			DateFormat dayFormat = new SimpleDateFormat("yy-MM-dd");
+			Date curDate = dateFormat.parse(dayFormat.format(new Date()) + " " + time);
+			return curDate.getTime();
+		} catch (ParseException e) {
+			e.printStackTrace();
+		}
+		return 0;
+	}
+
+	public static void main2(String[] args) {
+		System.out.println("Main Thread start at: " + new Date());
+		ExecutorService exec1 = Executors.newCachedThreadPool();
+		ExecutorService exec2 = Executors.newFixedThreadPool(5);
+		// 等价于ExecutorService exec3 = Executors.newFixedThreadPool(1);
+		ExecutorService exec3 = Executors.newSingleThreadExecutor();
+		ScheduledThreadPoolExecutor exec4 = (ScheduledThreadPoolExecutor) Executors.newScheduledThreadPool(10);
+		for (int i = 0; i < 10; i++) {
+			// exec1.execute(new Handle(String.valueOf(i)));
+			exec4.schedule(new Handle(String.valueOf(i)), 0, TimeUnit.SECONDS);
+		}
+		exec4.shutdown(); // 并不会马上关闭线程池
+		// exec1.shutdownNow() //立即关闭线程池
+		while (!exec4.isTerminated()) {
+			System.out.println("All tasks to finish...");
+		}
+		System.out.println("Main Thread end at: " + new Date());
+	}
 }
